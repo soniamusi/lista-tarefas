bd4e6b094cbecbf79c385c5de172f60941afeb89
Sonia Muniz <soninhamusi@gmail.com>
2024-08-25 12:15:45 -0400

    estilizacao dos butoes

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,13 @@ import "./Main.css";
 //FORM
 import { FaPlus } from "react-icons/fa";
 
+//TAREFAS
+import { FaEdit, FaWindowClose } from "react-icons/fa";
+
 export default class Main extends Component {
   state = {
     novaTarefa: "",
+    tarefas: ["Fazer cafe", "Beber agura", "Ir para academia"],
   };
 
   handleChange = (e) => {
@@ -16,7 +20,7 @@ export default class Main extends Component {
   };
 
   render() {
-    const { novaTarefa } = this.state;
+    const { novaTarefa, tarefas } = this.state;
     return (
       <div className="main">
         <h1>Lista de Tarefas</h1>
@@ -27,6 +31,18 @@ export default class Main extends Component {
             <FaPlus />
           </button>
         </form>
+
+        <ul className="tarefas">
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>
+              {tarefa}
+              <div>
+                <FaEdit className="edit" />
+                <FaWindowClose className="delete" />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
