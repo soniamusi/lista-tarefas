99c50c3e389c0dcc078263dff8f50ef8edea0a54
Sonia Muniz <soninhamusi@gmail.com>
2024-08-27 14:20:00 -0400

    armazenamento-localstorage

--- src/components/Main.js
+++ src/components/Main.js
@@ -14,6 +14,19 @@ export default class Main extends Component {
     index: -1,
   };
 
+  /* ARMAZENA NO LOCALSTORAGE do navegador*/
+  componentDidMount() {
+    const tarefas = JSON.parse(localStorage.getItem("tarefas"));
+    if (!tarefas) return;
+    this.setState({ tarefas });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { tarefas } = this.state;
+    if (tarefas == prevState.tarefas) return;
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  }
+
   /**INCLUINDO AS TAREFAS SUBMIT */
   handleSubmit = (e) => {
     e.preventDefault();
